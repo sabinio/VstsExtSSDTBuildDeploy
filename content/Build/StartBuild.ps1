b23160da33a72556d71ab033ae03686f7c0f08fe
Unknown <richie@bzzzt.io>
2017-11-29 23:53:08 +0000

    minor changes; download PoSHSSDTBuild module

--- content/Build/StartBuild.ps1
+++ content/Build/StartBuild.ps1
@@ -1,5 +1,20 @@
 
 
-.".\InstallMicrosoftDataToolsMSBuild.ps1"
-.".\InvokeMsBuildSSDT.ps1"
+# .".\InstallMicrosoftDataToolsMSBuild.ps1"
+# .".\InvokeMsBuildSSDT.ps1"
+# Install-MicrosoftDataToolsMSBuild -WorkingFolder $PSScriptRoot
+
 Install-MicrosoftDataToolsMSBuild -WorkingFolder $PSScriptRoot
+
+try {
+    Find-Module -Name "PoshSSDTBuildDeploy"
+}
+catch {
+    Write-Host "No PoshSSDTBuildDeploy" 
+    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+    Install-Module PoshSSDTBuildDeploy -Force -Scope CurrentUser
+}
+finally {
+    Import-Module PoshSSDTBuildDeploy -Force
+    Write-Host " PoshSSDTBuildDeploy Imported"
+}
