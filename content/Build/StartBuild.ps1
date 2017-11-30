1a04a934ae524b87ae5fe9c5f4cbf265c889e7a8
Unknown <richie@bzzzt.io>
2017-11-30 00:11:11 +0000

    alter try catch
    move out of finally

--- content/Build/StartBuild.ps1
+++ content/Build/StartBuild.ps1
@@ -1,10 +1,4 @@
 
-
-# .".\InstallMicrosoftDataToolsMSBuild.ps1"
-# .".\InvokeMsBuildSSDT.ps1"
-# Install-MicrosoftDataToolsMSBuild -WorkingFolder $PSScriptRoot
-
-
 try {
     Find-Module -Name "PoshSSDTBuildDeploy"
 }
@@ -13,8 +7,6 @@ catch {
     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
     Install-Module PoshSSDTBuildDeploy -Force -Scope CurrentUser
 }
-finally {
-    Import-Module PoshSSDTBuildDeploy -Force
-    Write-Host "PoshSSDTBuildDeploy Imported"
-    Install-MicrosoftDataToolsMSBuild -WorkingFolder $PSScriptRoot
-}
+Import-Module PoshSSDTBuildDeploy -Force
+Write-Host "PoshSSDTBuildDeploy Imported"
+Install-MicrosoftDataToolsMSBuild -WorkingFolder $PSScriptRoot
